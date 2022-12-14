7a6663bfe3f678541466e1d44688c4f4a4386632
cr8br0ze <zichongmeng@gmail.com>
2022-12-14 07:29:29 -0800

    finished project 0

--- proj0/planet.java
+++ proj0/planet.java
@@ -1,4 +1,97 @@
-package audio;
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public static final double G = 6.67e-11;
 
-public class planet {
+    /** Creating planet constructor and initialization */
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img){
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    /** calcDistance */
+    public double calcDistance(Planet b){
+        double dx = this.xxPos - b.xxPos;
+        double dy = this.yyPos - b.yyPos;
+        double rsquare = dx*dx + dy*dy;
+        return Math.sqrt(rsquare);
+    }
+
+    /** calcForceExertedBy */
+    public double calcForceExertedBy(Planet b){
+        double m1 = this.mass;
+        double m2 = b.mass;
+        double rsquare = this.calcDistance(b)*this.calcDistance(b);
+        return (this.G*m1*m2)/rsquare;
+    }
+
+    /** calcForceExertedByX and calcForceExertedByY */
+    public double calcForceExertedByX(Planet b){
+        double F = this.calcForceExertedBy(b);
+        double dx = b.xxPos - this.xxPos;
+        double r = this.calcDistance(b);
+        return (F*dx)/r;
+    }
+
+    public double calcForceExertedByY(Planet b){
+        double F = this.calcForceExertedBy(b);
+        double dy = b.yyPos - this.yyPos;
+        double r = this.calcDistance(b);
+        return (F*dy)/r;
+    }
+
+    /** calcNetForceExertedByX and calcNetForceExertedByY */
+    public double calcNetForceExertedByX(Planet[] Planetlist){
+        double Fnetx = 0;
+        for (int i = 0; i < Planetlist.length; i++){
+            if(Planetlist[i].equals(this)){
+                continue;
+            }
+            Fnetx += this.calcForceExertedByX(Planetlist[i]);
+        }
+        return Fnetx;
+    }
+    public double calcNetForceExertedByY(Planet[] Planetlist){
+        double Fnety = 0;
+        for (int i = 0; i < Planetlist.length; i++){
+            if(Planetlist[i].equals(this)){
+                continue;
+            }
+            Fnety += this.calcForceExertedByX(Planetlist[i]);
+        }
+        return Fnety;
+    }
+
+    /** update */
+    public void update(double time, double xforce, double yforce){
+        double ax = xforce/this.mass;
+        double ay = yforce/this.mass;
+        this.xxVel += time*ax;
+        this.yyVel += time*ay;
+        this.xxPos += time*this.xxVel;
+        this.yyPos += time*this.yyVel;
+    }
+
+    /** Drawing One Planet */
+    public void draw(){
+        StdDraw.picture(this.xxPos, this.yyPos, "images/"+this.imgFileName);
+    }
 }
